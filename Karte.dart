2b62014adb12b377ab5ad2804a30a7a676bc8526
Shad0wGam3r66 <tristan.seebold@altesgymnasium.eu>
2026-06-25 10:08:07 +0200

    Enhance Karte with interactive markers and dialogs
    Added multiple markers with dialog interactions for various locations.

--- Karte.dart
+++ Karte.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:test_1/materials/buttons.dart';
+import 'package:test_1/materials/Archivbuttons.dart';
+
 
 class Karte extends StatelessWidget {
   const Karte({Key? key}) : super(key: key);
@@ -20,14 +23,90 @@ class Karte extends StatelessWidget {
         MarkerLayer(
           markers: [
             Marker(
-              point: LatLng(53.1435001, 8.2146),
+              point: LatLng(53.14708, 8.21472),
               width: 80,
               height: 80,
-              child: Image.asset('bilder/marker.png'),
+              child: IconButton(
+                icon: Image.asset('bilder/Marker.png', width: 100, height: 100),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      content: Kaserne(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Marker(
+              point: LatLng(53.14196,  8.20809),
+              width: 80,
+              height: 80,
+              child: IconButton(
+                icon: Image.asset('bilder/Marker.png', width: 100, height: 100),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      content: Synagoge(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Marker(
+              point: LatLng(53.13458,  8.21499),
+              width: 80,
+              height: 80,
+              child: IconButton(
+                icon: Image.asset('bilder/Marker.png', width: 100, height: 100),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      content: Gefaengnis(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Marker(
+              point: LatLng(53.14411, 8.22311),
+              width: 80,
+              height: 80,
+              child: IconButton(
+                icon: Image.asset('bilder/Marker.png', width: 100, height: 100),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      content: Bahnhof(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Marker(
+              point: LatLng(53.14080, 8.21575),
+              width: 80,
+              height: 80,
+              child: IconButton(
+                icon: Image.asset('bilder/Marker.png', width: 100, height: 100),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      content: Schule(),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
-      ],
-    );
+          ],
+        );
+      
+    
   }
 }
