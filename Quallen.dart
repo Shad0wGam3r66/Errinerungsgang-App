f39075dfd8cdb39e6390964f55cccecb4e88c015
DavidMalohn <david.malohn@altesgymnasium.eu>
2026-06-25 12:18:57 +0200

    Create Quallen page with images and navigation
    This file implements a Quallen page in a Flutter app, featuring an app bar, text, and multiple images.

---
+++ Quallen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:test_1/Pages/Startseite.dart';
+
+class Quallen extends StatelessWidget {
+  const Quallen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quallen'),
+        backgroundColor: Color.fromARGB(255, 247, 231, 136),
+        leading: IconButton(
+          icon: Image.asset('bilder/burgerMenu.png', width: 70, height: 70),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Start()),
+            );
+          },
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 72, 69, 64),
+      body: Column(
+        children: [
+          Text('Quellen sind eine Qual, deswegen sind hier Quallen.', style: TextStyle(
+            fontSize: 60,
+            color: Color.fromARGB(255, 255, 255, 255),
+          )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+              Image.asset('bilder/qualle-quallentanz.gif', width: 200, height: 200),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
