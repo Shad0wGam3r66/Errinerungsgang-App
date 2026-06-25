7f4a06f648d9206417c859b8fb9ff99dbace3903
DavidMalohn <david.malohn@altesgymnasium.eu>
2026-06-25 10:42:39 +0200

    Add biographies and images for Carl Röver and Samson Hirsch

--- Personenseiten.dart
+++ Personenseiten.dart
@@ -31,6 +31,53 @@ class seiteCarlRoever extends StatelessWidget {
           Text('Carl Roever', style: TextStyle(
             fontSize: 60,
           )),
+          SizedBox(height: 50),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Carl Röver war ein führender Nationalsozialist in Oldenburg und spielte eine wichtige Rolle beim Aufbau und der Festigung der NS-Herrschaft in der Region. Bereits früh trat er der NSDAP bei und wurde später Ministerpräsident des Freistaates Oldenburg sowie Gauleiter des Gaues Weser-Ems. Unter seiner Führung wurden politische Gegner verfolgt und die nationalsozialistische Ideologie in Verwaltung und Gesellschaft durchgesetzt.', 
+                    style: TextStyle(
+                      fontSize: 20,)
+                    ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('bilder/Carl_Roever_1.jpg', width: 300, height: 300),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('bilder/Carl_Roever_2.jpg', width: 300, height: 300),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Röver unterstützte die Ausgrenzung und Verfolgung der jüdischen Bevölkerung sowie anderer vom Regime verfolgter Gruppen. Während der Zeit des Nationalsozialismus gehörte er zu den einflussreichsten Politikern Nordwestdeutschlands. Er starb 1942 während des Zweiten Weltkrieg angeblich an einer "Lungenentzündung", wobei es auch Gerüchte, um einen Euthanasie-Mord beziehungsweise Suizid gibt.', 
+                    style: TextStyle(
+                      fontSize: 20,)
+                    ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
       bottomNavigationBar: BottomAppBar(
@@ -73,6 +120,53 @@ class seiteSamsonRaphaelHirsch extends StatelessWidget {
           Text('Samson Raphael Hirsch', style: TextStyle(
             fontSize: 60,
           )),
+          SizedBox(height: 50),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Samson Raphael Hirsch (1808–1888) war ein bedeutender Rabbiner und einer der einflussreichsten Vertreter der jüdischen Neo-Orthodoxie im 19. Jahrhundert. Von 1830 bis 1841 wirkte er als Landesrabbiner des Herzogtums Oldenburg. In dieser Zeit setzte er sich für eine Erneuerung des jüdischen Gemeindelebens ein, ohne dabei die traditionellen religiösen Grundlagen aufzugeben.', 
+                    style: TextStyle(
+                      fontSize: 20,)
+                    ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('bilder/SamsonRaphaelHirsch1.jpeg', width: 300, height: 300),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('bilder/SamsonRaphaelHirsch2.jpg', width: 300, height: 300),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: 
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Hirsch vertrat die Auffassung, dass sich jüdische Gläubige aktiv am gesellschaftlichen und kulturellen Leben ihrer Zeit beteiligen sollten, während sie gleichzeitig die Gebote und Traditionen des Judentums bewahren. Dieses Konzept wurde später unter dem Leitsatz „Tora im Derech Eretz“ („Tora im Einklang mit dem weltlichen Leben“) bekannt. Im Gegensatz zu den Reformrabbinern seiner Zeit strebte Hirsch keine grundlegende Veränderung religiöser Vorschriften an. Stattdessen wollte er zeigen, dass traditionelle jüdische Lebensweise und moderne Bildung miteinander vereinbar sind. Durch seine Schriften und sein Wirken prägte er das orthodoxe Judentum weit über Deutschland hinaus und gilt bis heute als eine seiner wichtigsten Persönlichkeiten.', 
+                    style: TextStyle(
+                      fontSize: 20,)
+                    ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
       bottomNavigationBar: BottomAppBar(
